db4cf48f9b276ab40162653c4fb816927148a9f1
christian <christian@geko.cloud>
2021-08-26 14:22:18 +0200

    1st commit

---
+++ 74h5n5.ino/74h5n5.ino.ino
@@ -0,0 +1,215 @@
+/*
+
+  Shift Register Example
+
+  Turning on the outputs of a 74HC595 using an array
+
+ Hardware:
+
+ * 74HC595 shift register
+
+ * LEDs attached to each of the outputs of the shift register
+
+ Keybindings
+Arduino: Upload Alt + Cmd + U or Alt + Ctrl + U
+Arduino: Verify Alt + Cmd + R or Alt + Ctrl + R
+Arduino: Rebuild IntelliSense Configuration Alt + Cmd + I or Alt + Ctrl + I
+
+
+ */
+//Pin connected to ST_CP of 74HC595
+// D8
+int latchPin = 15;
+//Pin connected to SH_CP of 74HC595
+// D5
+int clockPin = 14;
+////Pin connected to DS of 74HC595
+// D7
+int dataPin = 13;
+
+//holders for infromation you're going to pass to shifting function
+byte data;
+byte dataArray[10];
+
+void setup() {
+
+  //set pins to output because they are addressed in the main loop
+  pinMode(latchPin, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("Setup");
+
+  //Binary notation as comment
+  // dataArray[0] = 0xFF; //0b11111111
+  // dataArray[1] = 0xFE; //0b11111110
+  // dataArray[2] = 0xFC; //0b11111100
+  // dataArray[3] = 0xF8; //0b11111000
+  // dataArray[4] = 0xF0; //0b11110000
+  // dataArray[5] = 0xE0; //0b11100000
+  // dataArray[6] = 0xC0; //0b11000000
+  // dataArray[7] = 0x80; //0b10000000
+  // dataArray[8] = 0x00; //0b00000000
+
+
+
+  dataArray[0] = 0x80;  // 10000000
+  dataArray[1] = 0x40;  // 01000000
+  dataArray[2] = 0x20;  // 00100000
+  dataArray[3] = 0x10;  // 00010000
+  dataArray[4] = 0x8;   // 00001000
+  dataArray[5] = 0x4;   // 00000100
+  dataArray[6] = 0x2;   // 00000010
+  dataArray[7] = 0x1;   // 00000001
+  // dataArray[9] = 0xE0; //0b11100000
+  //function that blinks all the LEDs
+  //gets passed the number of blinks and the pause time
+
+  // blinkAll_2Bytes(2,500);
+}
+
+void loop() {
+
+  digitalWrite(latchPin, 0);
+  //move 'em out
+  // shiftOut(dataPin, clockPin, dataArray[5]);  
+  // digitalWrite(latchPin, 1);
+  
+  for (int j = 0; j < 8; j++) {
+    
+    data = dataArray[j];
+    digitalWrite(latchPin, 0);
+    shiftOut(dataPin, clockPin, data);
+    digitalWrite(latchPin, 1);
+    delay(300);
+  }
+  delay(2000);
+
+  // delay(2000);
+  
+  // digitalWrite(latchPin, 0);
+  // shiftOut(dataPin, clockPin, dataArray[5]);
+  // digitalWrite(latchPin, 1);
+
+  // delay(2000);
+  
+  // digitalWrite(latchPin, 0);
+  // shiftOut(dataPin, clockPin, dataArray[9]);
+  // digitalWrite(latchPin, 1);
+
+  // delay(2000);
+
+  // for (int j = 0; j < 10; j++) {
+  // for (int j = 0; j < 9; j++) {
+
+  //   data = dataArray[j];                //load the light sequence you want from array
+  //   digitalWrite(latchPin, 0);          //ground latchPin and hold low for as long as you are transmitting
+  //   shiftOut(dataPin, clockPin, data);  //move 'em out
+
+  //   //return the latch pin high to signal chip that it
+  //   //no longer needs to listen for information
+  //   digitalWrite(latchPin, 1);
+  //   delay(300);
+
+  // }
+}
+
+// the heart of the program
+void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
+
+  // This shifts 8 bits out MSB first,
+  // on the rising edge of the clock,
+  // clock idles low
+  // internal function setup
+  int i=0;
+  int pinState;
+  pinMode(myClockPin, OUTPUT);
+  pinMode(myDataPin, OUTPUT);
+
+  // clear everything out just in case to
+  // prepare shift register for bit shifting
+  digitalWrite(myDataPin, 0);
+  digitalWrite(myClockPin, 0);
+  
+  // for each bit in the byte myDataOut&#xFFFD;
+  // NOTICE THAT WE ARE COUNTING DOWN in our for loop
+  // This means that %00000001 or "1" will go through such
+  // that it will be pin Q0 that lights.
+  for (i=7; i>=0; i--)  {
+
+    digitalWrite(myClockPin, 0);
+    // if the value passed to myDataOut and a bitmask result
+    // true then... so if we are at i=6 and our value is
+    // %11010100 it would the code compares it to %01000000
+    // and proceeds to set pinState to 1.
+    if ( myDataOut & (1<<i) ) {
+      pinState= 1;
+    }
+    else {
+      pinState= 0;
+    }
+
+    //Sets the pin to HIGH or LOW depending on pinState
+    digitalWrite(myDataPin, pinState);
+    //register shifts bits on upstroke of clock pin
+    digitalWrite(myClockPin, 1);
+    //zero the data pin after shift to prevent bleed through
+    digitalWrite(myDataPin, 0);
+  }
+
+  //stop shifting
+  digitalWrite(myClockPin, 0);
+}
+
+//blinks the whole register based on the number of times you want to
+//blink "n" and the pause between them "d"
+//starts with a moment of darkness to make sure the first blink
+//has its full visual effect.
+void blinkAll_2Bytes(int n, int d) {
+
+  digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, 0);
+  shiftOut(dataPin, clockPin, 0);
+  digitalWrite(latchPin, 1);
+  delay(2);
+
+  for (int x = 0; x < n; x++) {
+
+    digitalWrite(latchPin, 0);
+    shiftOut(dataPin, clockPin, 255);
+    shiftOut(dataPin, clockPin, 255);
+    digitalWrite(latchPin, 1);
+    delay(d);
+    digitalWrite(latchPin, 0);
+    shiftOut(dataPin, clockPin, 0);
+    shiftOut(dataPin, clockPin, 0);
+    digitalWrite(latchPin, 1);
+    delay(d);
+  }
+}
+
+
+// void setup()
+// {
+//     // Common lines:-
+//     Serial.begin(115200);
+//     byte val = 16;
+
+//     // 2124 program / 182 RAM:-
+//     for (int i = 0; i < 8; i++)
+//     {
+//         bool b = val & 0x80;
+//         Serial.print(b);
+//         val = val << 1;
+//     }
+
+//     // or
+
+//     // 2128 program / 182 RAM:-
+//     for (int i = 7; i >= 0; i--)
+//     {
+//         bool b = bitRead(val, i);
+//         Serial.print(b);
+//     }
+// }
+
+// void loop(){}
